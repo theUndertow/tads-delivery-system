6116cad772ae471ce2702e85eb8e0ac55248c9a0
marco <marco@zeus>
2019-05-16 13:45:41 -0300

    Criação dos métodos principais dos DAO's referente aos models

--- src/main/java/com/dac/tads/dao/CidadeDAO.java
+++ src/main/java/com/dac/tads/dao/CidadeDAO.java
@@ -5,10 +5,95 @@
  */
 package com.dac.tads.dao;
 
+import com.dac.tads.model.Cidade;
+import com.dac.tads.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author marco
  */
 public class CidadeDAO {
     
+    public boolean insertCidade(Cidade cidade) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(cidade);
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    // Retornará um único cidade
+    public Cidade selectCidade(int id) {
+        Cidade cidade = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query = session.createQuery(
+                    "from Cidade where Cidade.id = :id");
+            query.setInteger("id", id);
+            cidade = (Cidade) query.uniqueResult();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return cidade;
+    }
+    
+    // Retorna uma lista de todos os cidades
+    public List<Cidade> selectListCidade() {
+        List<Cidade> cidades;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("from Cidade");
+            cidades = query.list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return cidades;
+    }
+    
+    // Retorna um boolean em relação ao resultado do update
+    public boolean updateCidade(Cidade cidade) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(cidade);
+            session.getTransaction().commit();
+            session.clear();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+    // Retorna um boolean em relação a deleção de um cidade
+    public boolean deleteCidade(Cidade cidade) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction tx = session.beginTransaction();
+            session.delete(cidade);
+            tx.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/com/dac/tads/dao/UsuarioDAO.java
+++ src/main/java/com/dac/tads/dao/UsuarioDAO.java
@@ -5,10 +5,96 @@
  */
 package com.dac.tads.dao;
 
+import com.dac.tads.model.Usuario;
+import com.dac.tads.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author marco
  */
 public class UsuarioDAO {
     
+    
+    public boolean insertUsuario(Usuario usuario) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(usuario);
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    // Retornará um único usuario
+    public Usuario selectUsuario(int id) {
+        Usuario usuario = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query = session.createQuery(
+                    "from Usuario where Usuario.id = :id");
+            query.setInteger("id", id);
+            usuario = (Usuario) query.uniqueResult();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return usuario;
+    }
+    
+    // Retorna uma lista de todos os usuarios
+    public List<Usuario> selectListUsuario() {
+        List<Usuario> usuarios;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("from Usuario");
+            usuarios = query.list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return usuarios;
+    }
+    
+    // Retorna um boolean em relação ao resultado do update
+    public boolean updateUsuario(Usuario usuario) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(usuario);
+            session.getTransaction().commit();
+            session.clear();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+    // Retorna um boolean em relação a deleção de um usuario
+    public boolean deleteUsuario(Usuario usuario) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction tx = session.beginTransaction();
+            session.delete(usuario);
+            tx.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
